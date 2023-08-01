7d9e30725c3dc19813debf73e373c1c23e00507f
A-Tech-Services <abiodunoluwapelumi859@gmail.com>
2023-08-01 21:05:37 +0100

    new project

---
+++ src/components/navbar/NavBar.jsx
@@ -0,0 +1,16 @@
+import "./NavBar.css";
+
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <div className="nav-bar">
+            <div className="logo">
+                <i class="fa-solid fa-car"></i>
+                <h2>A-Trip</h2>
+            </div>
+    </div>
+  )
+}
+
+export default NavBar
